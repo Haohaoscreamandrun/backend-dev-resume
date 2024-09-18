57e137bd3f3215f47a6e687a044165796358ee5b
“ChenHao” <j842671395@gmail.com>
2024-09-18 11:11:17 +0800

    FEATS: version2 REASON:技能關鍵字多列一些，且分類處理，可能包含語言框架套件，雲端服務的細項，各種用過的技術工具例如 Linter，未來也根據職缺需求微調去配合。 由於你的過往經歷完全不相關，專業領域的內容我們基本看不懂，寫太細沒有意義，請再濃縮，用一到兩句話描述你覺得一般人都能看懂的東西。 專案作品用一句話講網站的主要功能就好。然後要列點談技術，每一點就是寫用什麼技術完成什麼事情，目前都只有功能描述。 調整順序，經歷完全不相關不要第一時間顯示，專案很重要，不要還要讓人點標籤才能切換，很多人不一定會注意到有可以點，學歷台大或許還有一點點用，可能比經歷還重要。

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
       backgroundImage:{
         'modal-pattern': "url('./IMG_1260.jpg')",
         'cv-avatar': "url('./cv-avatar.jpg')",
+        'wehelp-logo':"url('./wehelp_logo.png')",
         'corteva-logo':"url('./corteva_logo.jpg')",
         'basf-logo': "url('./BASF-Logo.jpg')",
         'ntu-logo':"url('./ntu_logo.jpg')",
         'nchu-logo':"url('./nchu_logo.jpg')",
         'parkinglot-logo': "url('./parkinglot_logo.png')",
+        'taipeidaytrip-logo': "url('./taipei_icon.png')",
         'weatherapp-logo':"url('./weatherapp_logo.webp')"
       },
       // backgroundSize:{
